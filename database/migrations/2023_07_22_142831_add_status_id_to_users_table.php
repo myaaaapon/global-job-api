d682880134232107ce86c67086f8c2a7e05b870b
myaaaapon <j272086j@myjuen.jp>
2023-07-23 19:03:42 +0900

    GLOBALJOB-1 PR作成微修正

--- database/migrations/2023_07_22_142831_add_status_id_to_users_table.php
+++ database/migrations/2023_07_22_142831_add_status_id_to_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Domain\Entities\UserStatus;
 
 class AddStatusIdToUsersTable extends Migration
 {
@@ -14,7 +15,7 @@ class AddStatusIdToUsersTable extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('status_id')->default(1);
+            $table->unsignedBigInteger('status_id')->default(UserStatus::FREE_USER);
             $table->foreign('status_id')->references('id')->on('user_statuses')->onDelete('restrict');
         });
     }
